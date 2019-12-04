518344308433891018208434f50a2249cc760d98
stelmistery <stelmistery@yandex.ru>
2019-12-05 02:30:58 +0300

    Added apps: products and accounts

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'producthunt',
+        'USER': 'postgres',
+        'PASSWORD': 'admin1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
