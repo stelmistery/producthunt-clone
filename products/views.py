77abafa382ed0fbc40b8cbeedb4a25d385187e09
stelmistery <stelmistery@yandex.ru>
2019-12-08 14:27:59 +0300

    Finaly v1.0

--- products/views.py
+++ products/views.py
@@ -9,7 +9,7 @@ def home(request):
     return render(request, 'products/home.html', {'products': products})
 
 
-@login_required
+@login_required(login_url='/accounts/login')
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['image']:
@@ -38,7 +38,8 @@ def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product': product})
 
-@login_required
+
+@login_required(login_url='/accounts/login')
 def upvote(request, product_id):
     if request.method == 'POST':
         product = get_object_or_404(Product, pk=product_id)
